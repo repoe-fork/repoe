2b73c6c62d106a5175c8901ed89c8c97d2c9f012
Taylor Kimmett <tkimmett@outlook.com>
2025-03-16 10:07:11 -0400

    generate json for keywords and their definitions

---
+++ RePoE/parser/poe2/keywords.py
@@ -0,0 +1,14 @@
+from RePoE.parser.util import call_with_default_args, write_any_json, write_json
+from RePoE.parser import Parser_Module
+
+
+class keywords(Parser_Module):
+    def write(self) -> None:
+        keywords = {
+            row["Id"]: row["Definition"] for row in self.relational_reader["KeywordPopups.dat64"]
+        }
+        write_any_json(keywords, self.data_path, "keywords")
+
+
+if __name__ == "__main__":
+    call_with_default_args(keywords)
