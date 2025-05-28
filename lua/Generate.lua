5cf73888073ad18143980810d689b67940967d46
ivy <119665497+lvlvllvlvllvlvl@users.noreply.github.com>
2025-05-28 10:22:47 +0700

    fix some lua errors

--- lua/Generate.lua
+++ lua/Generate.lua
@@ -8,7 +8,12 @@ latestTreeVersion = '0_0'
 launch = {}
 
 function LoadModule(module, ...)
-    return loadfile(path .. "/src/Data/" .. module .. ".lua")(...)
+    local loaded, error = loadfile(path .. "/src/" .. module .. (module:find(".lua") and "" or ".lua"))
+    if error or not loaded then
+        print("error loading module", error)
+    else
+        return loaded(...)
+    end
 end
 
 function triangular(n)
@@ -43,6 +48,43 @@ function isValueInArray(tbl, val)
     end
 end
 
+function sanitiseText(text)
+    if not text then return nil end
+    -- Something something unicode support something grumble
+    -- Only do these replacements if a char from 128-255 or '<' is found first
+    return text:find("[\128-\255<]") and text
+        :gsub("%b<>", "")
+        :gsub("\226\128\144", "-") -- U+2010 HYPHEN
+        :gsub("\226\128\145", "-") -- U+2011 NON-BREAKING HYPHEN
+        :gsub("\226\128\146", "-") -- U+2012 FIGURE DASH
+        :gsub("\226\128\147", "-") -- U+2013 EN DASH
+        :gsub("\226\128\148", "-") -- U+2014 EM DASH
+        :gsub("\226\128\149", "-") -- U+2015 HORIZONTAL BAR
+        :gsub("\226\136\146", "-") -- U+2212 MINUS SIGN
+        :gsub("\195\164", "a")     -- U+00E4 LATIN SMALL LETTER A WITH DIAERESIS
+        :gsub("\195\182", "o")     -- U+00F6 LATIN SMALL LETTER O WITH DIAERESIS
+        -- single-byte: Windows-1252 and similar
+        :gsub("\150", "-")         -- U+2013 EN DASH
+        :gsub("\151", "-")         -- U+2014 EM DASH
+        :gsub("\228", "a")         -- U+00E4 LATIN SMALL LETTER A WITH DIAERESIS
+        :gsub("\246", "o")         -- U+00F6 LATIN SMALL LETTER O WITH DIAERESIS
+        -- unsupported
+        :gsub("[\128-\255]", "?")
+        or text
+end
+
+function tableConcat(t1, t2)
+    local t3 = {}
+    for i = 1, #t1 do
+        t3[#t3 + 1] = t1[i]
+    end
+    for i = 1, #t2 do
+        t3[#t3 + 1] = t2[i]
+    end
+    return t3
+end
+
+require(path .. ".src.GameVersions")
 require(path .. ".src.Data.Global")
 
 local function makeSkillMod(modName, modType, modVal, flags, keywordFlags, ...)
@@ -61,22 +103,24 @@ end
 local function makeSkillDataMod(dataKey, dataValue, ...)
     return makeSkillMod("SkillData", "LIST", { key = dataKey, value = dataValue }, 0, 0, ...)
 end
-local function clean(map, visited, keys, path)
+local function clean(map, visited, keys, jpath)
     if type(map) == 'table' then
         for k, v in pairs(map) do
             keys[k] = k
-            if type(v) == 'function' then
+            if type(k) == "table" or type(k) == "function" then
+                map[k] = nil
+            elseif type(v) == 'function' then
                 map[k] = nil
             elseif type(v) == 'table' then
                 local seen = visited[v]
-                visited[v] = path and path .. "/" .. k or true
+                visited[v] = jpath and jpath .. "/" .. k or true
                 if seen == true then
                     map[k] = nil
-                elseif seen and path:find(seen) then
+                elseif seen and jpath:find(seen) then
                     -- recursive reference
                     map[k] = { ["$ref"] = seen }
                 else
-                    clean(v, visited, keys, path and path .. "/" .. k)
+                    clean(v, visited, keys, jpath and jpath .. "/" .. k)
                 end
             end
         end
@@ -86,10 +130,6 @@ end
 
 local json = require("dkjson")
 if file == "Global" then
-    require(path .. ".src.Modules.Data")
-    clean(data, {}, {})
-    io.open((params[2] or "data/") .. "DataModule.min.json", "w"):write(json.encode(data))
-    io.open((params[2] or "data/") .. "DataModule.json", "w"):write(json.encode(data, { indent = true }))
     return
 end
 
